140c6e4ed04fefd0eda68ff16464954dfc2941e6
mahiatm <77015554+mahiatm@users.noreply.github.com>
2025-10-23 23:28:22 -0400

    Update ElevatorIOReal.java

--- ElevatorIOReal.java
+++ ElevatorIOReal.java
@@ -29,25 +29,27 @@ public class ElevatorIOReal implements ElevatorIO{
     private StatusSignal<Current> elevCur;
 
     public ElevatorIOReal(){
-        elevMotor1 = new TalonFX(Ports.ELEVATOR_BOTTOM);
+        elevMotor1 = new TalonFX(Ports.ELEVATOR_BOTTOM); // These 2 lines initialize motor variables
         elevMotor2 = new TalonFX(Ports.ELEVATOR_TOP);
-        elevFollower = new Follower(elevMotor1.getDeviceID(), false);
+        elevFollower = new Follower(elevMotor1.getDeviceID(), false); 
 
-        elevVol = elevMotor1.getMotorVoltage();
+        elevVol = elevMotor1.getMotorVoltage(); // All variables just storing details about motors
         elevPos = elevMotor1.getPosition(); // Is actually inches
         elevVel = elevMotor1.getVelocity(); // Is actually inches/sec
         elevCur = elevMotor1.getStatorCurrent();
 
-        elevMotor1.getConfigurator().apply(ElevatorConstants.elev1Config);
+        elevMotor1.getConfigurator().apply(ElevatorConstants.elev1Config); //Applies Configurator settings from constants relating to PID, limits, etc.
         elevMotor2.getConfigurator().apply(ElevatorConstants.elev2Config);
 
-        BaseStatusSignal.setUpdateFrequencyForAll(50, elevVol, elevPos, elevVel, elevCur); 
-        ParentDevice.optimizeBusUtilizationForAll(elevMotor1,elevMotor2);
+        BaseStatusSignal.setUpdateFrequencyForAll(50, elevVol, elevPos, elevVel, elevCur); //sets the speed that all these variables that store the current status of the motors will be renewed
+        ParentDevice.optimizeBusUtilizationForAll(elevMotor1,elevMotor2); //optimizes the motors
         
-        elevMotor2.setControl(elevFollower);
+        elevMotor2.setControl(elevFollower); // makes the second motor follow the first?
     }
     
     @Override
+
+    //Stores all motor data, checks connection status of motors, refreshes all current motor details and renews it with the current details.
     public void updateInputs(ElevatorIOInputs inputs){
         var elev1Status = BaseStatusSignal.refreshAll(elevVol, elevPos, elevVel, elevCur);
 
@@ -60,22 +62,27 @@ public class ElevatorIOReal implements ElevatorIO{
     }
 
     @Override
-    public void setElevatorClosedLoop(double pos){ 
+    //Gives voltage to the motor based on feedback and other factors because its a closed loop
+    public void setElevatorClosedLoop(double pos){ //
         elevMotor1.setControl(position.withPosition(pos)); 
     }
 
     @Override
+    //Gives voltage to the motor moving the elevator effectively creating an open loop(moves without receiving feedback and doesn't adjust based on external circumstances)
     public void setElevatorOpenLoop(double volts){
         elevMotor1.setVoltage(volts);
     }
 
     @Override
+    //Sets the position of the elevator to the initial Start Position
     public void resetState() {
         elevMotor1.setPosition(ElevatorPosition.START.value); 
     }
 
     @Override
+    //Stops the voltage to the motor controlling the elevator
     public void stopElevator(){
         elevMotor1.stopMotor();
     }
 }
+
